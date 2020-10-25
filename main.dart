067beb95d6a5332193b5517cb0147bc7a0f3d014
tinpham43 <58009938+tinpham43@users.noreply.github.com>
2020-10-25 16:12:59 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -116,6 +116,15 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        materialTapTargetSize: MaterialTapTargetSize.padded,
+        backgroundColor: Colors.blueAccent,
+        mini: true,
+        child: Icon(
+          Icons.settings_sharp,
+        ),
+      ),
       endDrawer: SizedBox(
         width: 175.0,
         child: Drawer(
@@ -168,21 +177,131 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 IconButton(
-                  icon: Icon(Icons.home, color: Colors.green),
+                  icon: Icon(Icons.home_sharp, color: Colors.green),
                   onPressed: () {},
                 ),
                 IconButton(
-                  icon: Icon(Icons.search, color: Colors.green),
-                  onPressed: () {},
+                  icon: Icon(Icons.search_sharp, color: Colors.green),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondPage()),
+                    );
+                  },
                 ),
                 IconButton(
-                  icon: Icon(Icons.person, color: Colors.green),
-                  onPressed: () {},
+                  icon: Icon(Icons.person_sharp, color: Colors.green),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ThirdPage()),
+                    );
+                  },
                 )
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search"),
+      ),
+      bottomNavigationBar: Container(
+        height: 45.0,
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(width: 0.25, color: Colors.black),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home_sharp, color: Colors.green),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.search_sharp, color: Colors.green),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.person_sharp, color: Colors.green),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThirdPage()),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Placeholder"),
+      ),
+      bottomNavigationBar: Container(
+        height: 45.0,
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(width: 0.25, color: Colors.black),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home_sharp, color: Colors.green),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.search_sharp, color: Colors.green),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.person_sharp, color: Colors.green),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThirdPage()),
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
