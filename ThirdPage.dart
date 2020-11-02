ff3b63db6b21cd08998ce0ee506db1a6b199a33e
tinpham43 <58009938+tinpham43@users.noreply.github.com>
2020-11-02 15:03:36 -0700

    Update ThirdPage.dart

--- ThirdPage.dart
+++ ThirdPage.dart
@@ -19,13 +19,13 @@ class ThirdPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             IconButton(
-              icon: Icon(Icons.home_sharp, color: Colors.green),
+              icon: Icon(Icons.home_sharp, color: Colors.grey),
               onPressed: () {
                 Navigator.pop(context);
               },
             ),
             IconButton(
-              icon: Icon(Icons.search_sharp, color: Colors.green),
+              icon: Icon(Icons.search_sharp, color: Colors.grey),
               onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
@@ -36,6 +36,7 @@ class ThirdPage extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.person_sharp, color: Colors.green),
+              iconSize: 30.0,
               onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
