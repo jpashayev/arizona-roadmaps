4e25f18d4601827791664d514a53d97af89bd410
mhvalvur <48000311+mhvalvur@users.noreply.github.com>
2021-03-15 11:18:48 -0700

    Update main.dart
    Removed "Campsites Only" and "Discover" options from grid tile

--- main.dart
+++ main.dart
@@ -60,9 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
             padding: EdgeInsets.zero,
             children: <Widget>[
               CustomListTile(Icons.favorite_outline_sharp, 'Favorites', ()=>{}),
-              CustomListTile(Icons.sports_handball_sharp, 'Dominant Hand', ()=>{}),
-              CustomListTile(Icons.house_siding_sharp, 'Campsites Only', ()=>{}),
-              CustomListTile(Icons.explore_outlined, 'Discover', ()=>{}),
+              CustomListTile(Icons.sports_handball_sharp, 'Dominant Hand', ()=>{}), 
               CustomListTile(Icons.people_outline_sharp, 'About Us', ()=>{}),
               CustomListTile(Icons.live_help_sharp, 'Help', ()=>{}),
             ],
