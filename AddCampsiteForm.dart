cb8eb0f9e29c5ddc76695b72bfadb00b766ac0fc
tinpham43 <58009938+tinpham43@users.noreply.github.com>
2021-04-02 15:05:38 -0700

    Update AddCampsiteForm.dart

--- AddCampsiteForm.dart
+++ AddCampsiteForm.dart
@@ -20,58 +20,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter"),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: GestureDetector(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AddCampsiteForm(addCampsiteForm: _addCampsiteForm);
-                    });
-              },
-              child: Icon(
-                Icons.library_add,
-                size: 26.0,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AddCampsiteForm extends StatefulWidget {
-  final ValueChanged<String> addCampsiteForm;
-
-  AddCampsiteForm({this.addCampsiteForm});
-
-  @override
-  _AddCampsiteFormState createState() => _AddCampsiteFormState();
-}
-
-class _AddCampsiteFormState extends State<AddCampsiteForm> {
-  final databaseReference = FirebaseDatabase.instance.reference();
-  final _formKey = GlobalKey<FormState>();
-  final campNameController = TextEditingController();
-  final addressController = TextEditingController();
-
-  _addToDatabase() {
-	setState(() {
-	  databaseReference.child(8).set({
-		"Name": campNameController.text,
-		"Address": addressController.text,
-	  });
-	});
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return AlertDialog(
       content: Stack(
         clipBehavior: Clip.none,
@@ -97,7 +45,7 @@ class _AddCampsiteFormState extends State<AddCampsiteForm> {
                 Padding(
                   padding: EdgeInsets.all(8.0),
                   child: TextFormField(
-				    controller: campNameController,
+                    controller: campNameController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: "Campsite name",
@@ -111,46 +59,66 @@ class _AddCampsiteFormState extends State<AddCampsiteForm> {
                     },
                   ),
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: latController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Lat",
+                            hintText: "e.g. 40.01",
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter latitude";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: lngController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Lng",
+                            hintText: "e.g. 111.01",
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter longitude";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
                 Padding(
                   padding: EdgeInsets.all(8.0),
                   child: TextFormField(
-                    controller: addressController,
+                    controller: distanceController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: "Campsite address",
-                      hintText: "e.g. 1234 N 99th Ave",
+                      labelText: "Distance",
+                      hintText: "e.g. 4",
                     ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return "Please enter address";
+                        return "Please enter distance";
                       }
                       return null;
                     },
                   ),
                 ),
-		      Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("or"),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.green,
-                      onPrimary: Colors.white,
-                    ),
-                    child: Text("Use Current Location"),
-                    onPressed: () {
-                      setState(() {
-                        addressController.text = "User's current location here";
-                      });
-                    },
-                  ),
-                ),
-			  ],),
                 Padding(
                   padding: const EdgeInsets.only(top: 24.0),
                   child: ElevatedButton(
@@ -159,16 +127,7 @@ class _AddCampsiteFormState extends State<AddCampsiteForm> {
                       onPrimary: Colors.white,
                     ),
                     child: Text("Submit Campsite"),
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        this.widget.addCampsiteForm("yo");
-                        
-                        addToDatabase();
-                        
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text("Campsite submitted!")));
-                      }
-                    },
+                    onPressed: () {},
                   ),
                 )
               ],
