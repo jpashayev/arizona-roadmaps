c6dc72d364fec03da18a39a92599d1ccb2484002
jpashayev <40347155+jpashayev@users.noreply.github.com>
2021-04-02 23:21:42 -0700

    Add files via upload

--- CampsitePage.dart
+++ CampsitePage.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'BottomNav.dart';
 import 'SettingsDrawer.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'Campsite.dart';
-import 'campsiteRepository.dart';
+import 'CampsiteRepository.dart';
 import 'AddCampsiteForm.dart';
+
 
 class CampsitePage extends StatefulWidget {
   //Passed variables
@@ -22,7 +23,6 @@ class CampsitePage extends StatefulWidget {
 
 //create state of CampsitePage
 class _CampsitePageState extends State<CampsitePage> {
-
   //Declare & Initialize scaffoldKey to GlobalKey
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -44,12 +44,12 @@ class _CampsitePageState extends State<CampsitePage> {
   // [Remaining] button to add campsite in appbar
   Widget _buildCampsite(BuildContext context) {
     return Scaffold(
+
       //Global Scaffold Key
       key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Campsites"),
-        
-        //TO DO --> Call to method to (addCampsite)
+
         actions: <Widget>[
           Padding(
             padding: EdgeInsets.only(right: 15.0),
@@ -72,19 +72,20 @@ class _CampsitePageState extends State<CampsitePage> {
 
       //StreamBuilder for Querying Snapshots
       //return Loading Indicator if no data
-      //return call to buildList if data exists
-      body: StreamBuilder<QuerySnapshot> (
-        stream: repository.getStream(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return LinearProgressIndicator();
-          return _buildList(context, snapshot.data.documents);
-        }),
-          endDrawer: SizedBox(
-            width: 175.0,
-            child: SettingsDrawer(),
-          ),
-          bottomNavigationBar:
-              bottomNav(context, true, _scaffoldKey, "third", this.widget.search),
+      //return call to _buildList if data exists
+      body: StreamBuilder<QuerySnapshot>(
+          stream: repository.getStream(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return LinearProgressIndicator();
+            return _buildList(context, snapshot.data.documents);
+          }),
+      endDrawer: SizedBox(
+        width: 175.0,
+        child: SettingsDrawer(),
+      ),
+      bottomNavigationBar:
+          bottomNav(context, true, _scaffoldKey, "third", this.widget.search),
     );
   }
 
@@ -93,54 +94,65 @@ class _CampsitePageState extends State<CampsitePage> {
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
       padding: const EdgeInsets.all(10.0),
+
+      //map data for each item in list, into new list
       children: snapshot.map((data) => _buildListItem(context, data)).toList(),
     );
   }
 
+  //Build Each Campsite (ListItem)
+  //Card --> Column --> Expansion Tile
+  //Padding --> Tooltip --> Text
   Widget _buildListItem(BuildContext context, DocumentSnapshot snapshot) {
 
     //get campsites from snapshot
     final site = Campsite.fromSnapshot(snapshot);
 
     //return campsites on card
-      return new Card(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ExpansionTile(
-              title: Text(site.name),
-              trailing: Icon(Icons.add_road_outlined),
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(7.0),
-                  child: Text("Latitude: " + site.latlng.latitude.toString()),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(7.0),
-                  child: Text("Latitude: " + site.latlng.longitude.toString()),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    getCoordinates(snapshot);
-                    Navigator.pop(context);
-                  },
-                  child: Icon(Icons.search),
-                ),
-              ],
-            ),
-          ],
-        ),
-      );
+    return new Card(
+
+      //Column
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          //ExpansionTile
+          ExpansionTile(
+            title: Text("Campsite: " + site.name),
+            trailing: Icon(Icons.add_road_outlined),
+            children: [
+
+              //Distance Tooltip
+              Padding(
+                padding: EdgeInsets.all(7.0),
+                child: Tooltip(
+                    message: 'Distance Traveled on Dirt Road',
+                    child: Text("Trek: " + site.distance.toString())),
+              ),
+
+              //Create Route
+              //Calls getCoordinates and pops screen --> home
+              ElevatedButton(
+                onPressed: () {
+                  getCoordinates(site.latlng);
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.search),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   //Method to send Lat/Lng from GeoPoint to changeMarker
-  getCoordinates(DocumentSnapshot snapshot) {
-
-    //get campsites from snapshot
-    final site = Campsite.fromSnapshot(snapshot);
+  getCoordinates(GeoPoint latlng) {
 
     //Take current iteration and returns coordinates to new LatLng
-    LatLng pos = LatLng(site.latlng.latitude, site.latlng.longitude);
+    double lat = latlng.latitude;
+    double lng = latlng.longitude;
+    LatLng pos = LatLng(lat, lng);
 
     //Call changeMarker with new coordinates
     this.widget.changeMarker(pos);
