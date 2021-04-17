6e3ccce4b7b0498a2a3c5d51e27ca81e24a30771
mhvalvur <48000311+mhvalvur@users.noreply.github.com>
2021-04-17 13:50:14 -0700

    Update Favorites.dart
    potentially good now

--- Favorites.dart
+++ Favorites.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+
 class Favorites extends StatelessWidget{
-  String favName;
-  Favorites({this.favName});
+
+
+  List<String> favList;
+  Favorites({this.favList});
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
+
+      return Scaffold(
         appBar: AppBar(
           title: Text(
-              'Favorites',
+            'Favorites',
           ),
         ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.green,
-        ),
-        padding: const EdgeInsets.all(32),
-        child: Text(
-          favName,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
 
-          ),
-        ),
-      ),
-    );
-  }
-}
+        body: ListView(
+          children: [_buildList(context)],
+        )
+      );
+    }
+
+    Widget _buildList(BuildContext context){
+        for(String s in favList){
+          return new Card(
+            margin: EdgeInsets.all(6.0),
+            color: Colors.green,
+            child: ListTile(
+                title: Text("Campsite: " + s,
+                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                )
+            )
+          );
+        }
+    }
